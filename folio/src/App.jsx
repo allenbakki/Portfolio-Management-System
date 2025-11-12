b9b511ae1e9d2fe5ab86a76b36636bb847d9ce65
rmosad <ramiz.mosad@slu.edu>
2025-11-12 15:14:28 -0600

    created a simple portfolio template

--- folio/src/App.jsx
+++ folio/src/App.jsx
@@ -5,6 +5,8 @@ import SignIn from "./authentication/SignIn";
 import Landing from "./pages/landing/landing";
 import Portfolio from "./pages/portfolio/portfolio";
 import Account from "./pages/account";
+import TemplatePage from "./pages/templates/templatePage";
+
 import { useGlobalContext } from "./context/GlobalContext";
 
 function App() {
@@ -19,6 +21,7 @@ function App() {
         {!isLogggedIn && <Route path="/signUp" element={<SignIn />} />}
         {isLogggedIn && <Route path="/portfolio" element={<Portfolio />} />}
         {isLogggedIn &&   <Route path="/account" element={<Account />} />}
+        <Route path="/template" element={<TemplatePage />} />
         <Route path="/*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
